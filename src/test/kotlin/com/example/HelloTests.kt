cb9f4b8332606deae1cf2990824c3435c1f2b588
Luca Piccinelli <luca.picci@gmail.com>
2020-05-25 08:03:29 +0200

    test without restart

--- src/test/kotlin/com/example/HelloTests.kt
+++ src/test/kotlin/com/example/HelloTests.kt
@@ -1,14 +1,20 @@
 package com.example
 
+import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForObject
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = [MainApplication::class])
-class HelloTests {
+class HelloTests(@Autowired val restTemplate: TestRestTemplate) {
 
     @Test
-    fun test(){
+    fun `GIVEN springboot in certain state WHEN a restart happens THEN it should preserve the state`(){
+        restTemplate.getForObject<String>("/hello") shouldBe "Ok 0"
 
+        restTemplate.getForObject<String>("/hello") shouldBe "Ok 1"
     }
 
 }
