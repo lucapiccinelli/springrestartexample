af1a4b8e80d5d5a44cc4372b40600a86ee9546f6
Luca Piccinelli <luca.picci@gmail.com>
2020-05-27 08:18:24 +0200

    The final solution

--- src/test/kotlin/com/example/HelloTests.kt
+++ src/test/kotlin/com/example/HelloTests.kt
@@ -2,21 +2,28 @@ package com.example
 
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.runApplication
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.test.web.client.getForObject
+import kotlin.random.Random
 
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = [MainApplication::class])
-class HelloTests(@Autowired val restTemplate: TestRestTemplate) {
+class HelloTests {
 
     @Test
     fun `GIVEN springboot in certain state WHEN a restart happens THEN it should preserve the state`(){
-        restTemplate.getForObject<String>("/hello") shouldBe "Ok 0"
+        val restTemplate = TestRestTemplate()
+        var webserverPort = Random.nextInt(10000, 10100)
 
-        restart()
+        val appContext = SpringApplication.run(MainApplication::class.java, "--server.port=$webserverPort")
+        restTemplate.getForObject<String>("http://localhost:$webserverPort/hello") shouldBe "Ok 0"
 
-        restTemplate.getForObject<String>("/hello") shouldBe "Ok 1"
+        appContext.registerShutdownHook()
+
+        webserverPort = Random.nextInt(10000, 10100)
+        val newContext = SpringApplication.run(MainApplication::class.java, "--server.port=$webserverPort")
+        restTemplate.getForObject<String>("http://localhost:$webserverPort/hello") shouldBe "Ok 1"
+
+        newContext.registerShutdownHook()
     }
 }
